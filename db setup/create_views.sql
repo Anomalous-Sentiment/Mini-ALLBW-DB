1ad7aaa9d856e35bad94dd4ca71644a65f8afcca
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2024-07-25 05:14:58 +0800

    Updated view with more columns

--- db setup/create_views.sql
+++ db setup/create_views.sql
@@ -1,6 +1,6 @@
-DROP VIEW IF EXISTS memoria_skills;
-CREATE OR REPLACE VIEW memoria_skills AS
-    SELECT u_mem.en_name, mem.rarity, quest_sk.en_name AS "HUGE Skill", quest_sk.en_description AS "quest_desc", gvg_sk.en_name AS "Legion Skill", gvg_sk.en_description AS "gvg_desc", auto_sk.en_name AS "Support Skill", auto_sk.en_description AS "support_desc"
+DROP VIEW IF EXISTS memoria_list;
+CREATE OR REPLACE VIEW memoria_list AS
+    SELECT u_mem.unique_id, mem.card_mst_id, u_mem.en_name AS "name", mem.rarity, mem.card_type, mem.attribute, quest_sk.en_name AS "quest_sk", quest_sk.en_description AS "quest_desc", gvg_sk.en_name AS "gvg_sk", gvg_sk.en_description AS "gvg_desc", auto_sk.en_name AS "auto_sk", auto_sk.en_description AS "support_desc", mem.max_phys_atk, mem.max_phys_def, mem.max_mag_atk, mem.max_mag_def
     FROM memoria mem 
     INNER JOIN unique_memoria u_mem ON mem.unique_id = u_mem.unique_id
     INNER JOIN skills quest_sk ON quest_sk.skill_mst_id = mem.quest_skill_mst_id
